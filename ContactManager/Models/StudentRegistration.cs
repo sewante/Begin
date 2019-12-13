5ff7033e3cd3f4861ff6e3c783a3f09cea0674bc
Robert Mwesigye <mwesirob@gmail.com>
2019-12-13 18:27:36 +0300

    creating the Student models

--- ContactManager/Models/StudentRegistration.cs
+++ ContactManager/Models/StudentRegistration.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿/**
+ * AUTHOR: MWESIGYE Robert
+ * DATE: 13th Dec, 2019
+ * 
+ * This clase is a singleton class that will be responsible for Registering Students
+ * include all operations for getting a student, adding a student, getting all students and removing a student
+ 
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace ContactManager.Models
 {
+
     public class StudentRegistration
     {
         List<Student> studentList; // list to have all the students registered
@@ -41,12 +52,31 @@ namespace ContactManager.Models
                 if(student.RegistrationNumber.Equals(registrationNumber))
                 {
                     studentList.RemoveAt(i);    // update the students list
-                    return "Student with registration ID: " + registrationNumber + " deleted successfully";
+                    return "Student with registration number: " + registrationNumber + " deleted successfully";
                 }
             }
 
             return "Deleting un-successful";
         }
         // get all the students
+        public List<Student> GetAll()
+        {
+            return studentList;
+        }
+        // update a student's details
+        public String UpdateStudent(Student student)
+        {
+            for(int n = 0; n < studentList.Count; n++)
+            {
+                Student studentN = studentList.ElementAt(n);
+                if(studentN.RegistrationNumber.Equals(student.RegistrationNumber))
+                {
+                    studentList[n] = student;   // update this student
+                    return "Details for student with registration number: " + studentN.RegistrationNumber + " have benn updated successfully";
+                }
+            }
+            return "Update un-successful";
+        }
+
     }
 }
---
+++ ContactManager/Models/StudentRegistrationReply.cs
@@ -0,0 +1,44 @@
+﻿/**
+ * AUTHOR: MWESIGYE Robert
+ * DATE: 13th Dec, 2019
+ * 
+ * This class is responsible for sending response messages to the client applications which use the StudentRegistration class
+ 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+    public class StudentRegistrationReply
+    {
+
+        String name;
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        String registrationNumber;
+        public String RegistrationNumber
+        {
+            get { return registrationNumber; }
+            set { registrationNumber = value; }
+        }
+        String registrationStatus;
+        public String RegistrationStatus
+        {
+            get { return registrationStatus; }
+            set { registrationStatus = value; }
+        }
+    }
+}
