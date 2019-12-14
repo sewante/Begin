98bbf6c8ed9fcbdb0184668a5a526418532f3da5
Robert Mwesigye <mwesirob@gmail.com>
2019-12-14 15:47:16 +0300

    Delete and Put methods

---
+++ ContactManager/Controllers/StudentDeleteController.cs
@@ -0,0 +1,27 @@
+﻿/**
+ * AUTHOR: MWESIGYE Robert
+ * DATE: 14th Dec, 2019
+ *
+ * This class is responsible for handling all the delete requests of this API for the Students module
+ * It delete the student whose registration number is specified.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ContactManager.Models;
+
+namespace ContactManager.Controllers
+{
+    public class StudentDeleteController : ApiController
+    {
+        // method to delete the student details whose registration number is specified
+        public string DeleteStudentRecord(String registrationNumber)
+        {
+            return null;
+        }
+    }
+}
---
+++ ContactManager/Controllers/StudentUpdateController.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * AUTHOR: MWESIGYE Robert
+ * DATE: 14th Dec, 2019
+ *
+ * This class is responsible for handling all the put requests of this API for the Students module
+ * It updates the student details.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using ContactManager.Models;
+
+namespace ContactManager.Controllers
+{
+    public class StudentUpdateController : ApiController
+    {
+        // method to update the student's records
+        public string PutStudentRecord(Student student)
+        {
+            Console.WriteLine("In updateStudentRecord");
+
+            return StudentRegistration.getInstance().UpdateStudent(student);
+        }
+    }
+}
